84b20de0d82bac3fedc14a413983e8492865d77d
shrimanju <shrimanju61@gmail.com>
2021-02-07 18:45:46 +0530

    useffect

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import  Footer from './Footer';
 import {db} from "./firebase";
   export  function Contact() {
